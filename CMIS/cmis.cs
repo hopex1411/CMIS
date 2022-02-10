35617732201f5f0ce4f050336a07da2d74e08cb3
Anton ulrik <42369351+hopex1411@users.noreply.github.com>
2022-02-10 08:29:40 +0100

    created and updated project

--- CMIS/cmis.cs
+++ CMIS/cmis.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace CMIS
+namespace cmis
 {
     class cmis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
         }
     }
 }
