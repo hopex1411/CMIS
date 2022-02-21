f772dc0072ca33ca3ea1433ad4755cd35639848d
Anton ulrik <42369351+hopex1411@users.noreply.github.com>
2022-02-21 12:26:28 +0100

    added siw's styling

--- CMIS/cmis.cs
+++ CMIS/cmis.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace cmis
 {
@@ -14,19 +15,22 @@ namespace cmis
             // Fejlmeddelser
             // Gør programmet mere lækkert(visuelt)
 
-            Directory.CreateDirectory(@"C:\cmisData");  
+            Directory.CreateDirectory(@"C:\cmisData");
             string myLine = "";
             string myPath = @"C:\cmisData\database.txt";
             string menuAnswer;
             // arrayet myTxt contains menuItems
             string[] myTxt = { "Telefon", "Fornavn", "Efternavn", "Gade/Vej", "Husnummer", "Postnr", "Bynavn", "Email" };
             string[] myValues = new string[myTxt.Length];
+            string yesNo = "";
             int counter = 0;
+
 
             do
             {
                 // Main menu,
-                Console.WriteLine("[O] Opret [F] Find [V] Vis alle [Q] Afslut :");
+                Console.WriteLine("\n[O] Opret   [F] Find   [V] Vis alle   [Q] Afslut :");
+                Console.Write("\nVælg funktion : ");
                 menuAnswer = Console.ReadLine();
 
 
@@ -42,7 +46,7 @@ namespace cmis
                             Console.Write(myTxt[myRecord] + ": ");
                             myValues[myRecord] = Console.ReadLine();
                         }
-                        Console.WriteLine("Tak - du har indtastet : ");
+                        Console.WriteLine("\nTak - du har indtastet : ");
 
                         // Outputs the data the user inputted
                         for (int myRecord = 0; myRecord < myTxt.Length; myRecord++)
@@ -51,24 +55,45 @@ namespace cmis
                         }
 
                         myLine = myLine + "\n";
-
                         Console.WriteLine(myLine);
-                        Console.WriteLine("\n Skal data skrives til filen ?? j/n");
-                        string yesNo = Console.ReadLine();
-                        if (yesNo.ToLower() == "j")
+                        do
                         {
-                            // Takes the data the user has inputted, and puts in the database
-                            File.AppendAllText(myPath, myLine, Encoding.Unicode);
-                            Console.WriteLine("Oplysninger gemmes ......");
-                        }
-                        myLine="";
+
+                            Console.WriteLine("\n Skal data skrives til filen ?? j/n");
+                            yesNo = Console.ReadLine();
+                            if (yesNo.ToLower() != "j" && yesNo.ToLower() != "n")
+                            {
+                                Console.WriteLine("Ugyldig indtastning");
+                            }
+
+                            if (yesNo.ToLower() == "j")
+                            {
+                                // Takes the data the user has inputted, and puts in the database
+                                File.AppendAllText(myPath, myLine, Encoding.Unicode);
+                                Console.Write("\nOplysninger gemmes ......");
+                                Thread.Sleep(2000);
+                                Console.WriteLine("\n\nOplysninger gemt");
+                                Thread.Sleep(1000);
+                                Console.Clear();
+                            }
+
+                        } while (yesNo.ToLower() != "j" && yesNo.ToLower() != "n");
+
+
+
+                        myLine = "";
+
                     }
                     else
                     {
                         using (StreamWriter sw = File.AppendText(myPath))
                         {
                         }
-                        Console.WriteLine("Der er ingen database...... Database oprettet");
+                        Console.Write("\nDer er ingen database...... Database oprettes");
+                        Thread.Sleep(2000);
+                        Console.WriteLine("\nDatabase oprettet");
+                        Thread.Sleep(3000);
+                        Console.Clear();
                     }
 
 
@@ -76,6 +101,7 @@ namespace cmis
                 // User search
                 if (menuAnswer.ToLower() == "f")
                 {
+                    Console.Clear();
                     if (File.Exists(myPath))
                     {
                         // Reads all data in the database
@@ -104,12 +130,15 @@ namespace cmis
                         }
                         Console.WriteLine("Der er ingen database...... Database oprettet");
                     }
+                    Console.WriteLine("\n\n\nTryk på en tast for at åbne menuen");
+                    Console.ReadKey();
 
                 }
 
                 // Full database output
                 if (menuAnswer.ToLower() == "v")
                 {
+                    Console.Clear();
                     if (File.Exists(myPath))
                     {
                         string[] myDatabase = File.ReadAllLines(myPath);
@@ -117,15 +146,15 @@ namespace cmis
                         {
                             Console.WriteLine(line);
                             counter++;
-                            if(counter % 15 == 0)
+                            if (counter % 15 == 0)
                             {
                                 Console.WriteLine("Tryk for at se flere");
                                 Console.ReadKey();
                                 Console.Clear();
                             }
-                            
+
                         }
-                        
+
                     }
                     else
                     {
@@ -138,8 +167,10 @@ namespace cmis
                     {
                         Console.WriteLine("databasen er tom");
                     }
+                    Console.WriteLine("\n\n\nTryk på en tast for at åbne menuen");
+                    Console.ReadKey();
                 }
-                if(menuAnswer.ToLower()!="o"&&menuAnswer.ToLower()!="f"&&menuAnswer.ToLower()!="v"&&menuAnswer.ToLower()!="q")
+                if (menuAnswer.ToLower() != "o" && menuAnswer.ToLower() != "f" && menuAnswer.ToLower() != "v" && menuAnswer.ToLower() != "q")
                 {
                     Console.WriteLine("Du har indtastet et ugyldigt tegn.");
                 }
